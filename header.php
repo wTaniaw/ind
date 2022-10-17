b92359a87b24929f2ddd23f3d3af9c65dcf80eb8
Tania <ttania_torres@hotmail.com>
2022-10-17 08:18:19 -0600

    asdasd

--- header.php
+++ header.php
@@ -8,98 +8,109 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 <html>
 
 <head>
-	<title>Instituto Interdisciplinario de Estudios Educativos y Organizacionales</title>
-	<!--/tags -->
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<meta name="keywords" content="instituto,interdisciplinario,maestrías,escuela,estudios,superior,docencia,psicología,educativa,gestión,politicas,publicas,estudiar,instituto interdisciplinario de estudios educativos y organizacionales" />
+    <title>Instituto Interdisciplinario de Estudios Educativos y Organizacionales</title>
+    <!--/tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="keywords"
+        content="instituto,interdisciplinario,maestrías,escuela,estudios,superior,docencia,psicología,educativa,gestión,politicas,publicas,estudiar,instituto interdisciplinario de estudios educativos y organizacionales" />
 
-	<!--//tags -->
-	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
-	<link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
-	<link rel="stylesheet" href="css/owl.theme.css" type="text/css" media="all">
-	<link href="css/font-awesome.css" rel="stylesheet">
-	<link rel="stylesheet" href="css/animate.css">
-	<!-- //for bootstrap working -->
+    <!--//tags -->
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css" media="all">
+    <link rel="stylesheet" href="css/owl.theme.css" type="text/css" media="all">
+    <link href="css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href="https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <script>
+    addEventListener("load", function() {
+        setTimeout(hideURLbar, 0);
+    }, false);
 
-	<link href="https://fonts.googleapis.com/css?family=Muli:200,200i,300,300i,400,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-	<script>
-		addEventListener("load", function() {
-			setTimeout(hideURLbar, 0);
-		}, false);
-
-		function hideURLbar() {
-			window.scrollTo(0, 1);
-		}
-	</script>
+    function hideURLbar() {
+        window.scrollTo(0, 1);
+    }
+    </script>
 </head>
 
 <body class="">
-	<!-- header -->
-	<div class="header animatedParent" id="menu">
-		<div class="agileits_top_menu animated bounceInDown">
-			<div class="w3l_header_left">
-				<ul>
-					<li><i class="fa fa-phone" aria-hidden="true"></i> 614 415 5089</li>
-					<li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a href="mailto:info@iineed.com.mx">info@iineed.com.mx</a></li>
-				</ul>
-			</div>
-			<div class="w3l_header_right">
-				<div class="w3ls-social-icons text-left">
-					<a target="_blank" class="facebook" target="_blank" href="https://www.facebook.com/iineedmaestrias"><i class="fa fa-facebook"></i><span style="color:white"> Chihuahua</span></a>
-					<a target="_blank" class="facebook" href="https://www.facebook.com/Iineed-Plantel-Ju%C3%A1rez-714123785332571/"><i class="fa fa-facebook"></i><span style="color:white"> Juárez</span></a>
+    <!-- header -->
+    <div class="header animatedParent" id="menu">
+        <div class="agileits_top_menu animated bounceInDown">
+            <div class="w3l_header_left">
+                <ul>
+                    <li><i class="fa fa-phone" aria-hidden="true"></i> 614 415 5089</li>
+                    <li><i class="fa fa-envelope-o" aria-hidden="true"></i> <a
+                            href="mailto:info@iineed.com.mx">info@iineed.com.mx</a></li>
+                </ul>
+            </div>
+            <div class="w3l_header_right">
+                <div class="w3ls-social-icons text-left">
+                    <a target="_blank" class="facebook" target="_blank"
+                        href="https://www.facebook.com/iineedmaestrias"><i class="fa fa-facebook"></i><span
+                            style="color:white"> Chihuahua</span></a>
+                    <a target="_blank" class="facebook"
+                        href="https://www.facebook.com/Iineed-Plantel-Ju%C3%A1rez-714123785332571/"><i
+                            class="fa fa-facebook"></i><span style="color:white"> Juárez</span></a>
 
-				</div>
-			</div>
-			<div class="clearfix"> </div>
-		</div>
-		<div class="content white agile-info">
-			<nav class="navbar navbar-default" role="navigation">
-				<div class="container ">
-					<div class="navbar-header">
-						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-							<span class="sr-only">Toggle navigation</span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-							<span class="icon-bar"></span>
-						</button>
-						<a class="navbar-brand" href="index.php">
-							<img src="images/logo.png" width="150px"><br><br>
-						</a>
-					</div>
-					<!--/.navbar-header-->
-					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-						<nav class="link-effect-2" id="link-effect-2">
-							<ul class="nav navbar-nav">
-								<li class="active  animated rotateInUpLeft"><a href="index.php" class="effect-3">Inicio</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle effect-3" data-toggle="dropdown">Posgrados <b class="caret"></b></a>
-									<ul class="dropdown-menu" style="font-size: 1.1em;font-weight: 500">
-										<li class=""><a href="md2.php" class="">Maestría en Docencia</a></li>
-										<li class=""><a href="md2l.php" class="">Maestría en Docencia en linea</a></li>
-										<li><a href="mpe2.php">Maestría en Psicología Educativa</a></li>
-										<li><a href="mgpp2.php">Maestría en Gestión de Políticas Públicas</a>
-										</li>
-									</ul>
-								</li>
-								<li><a href="consultoria.php" class="effect-3">Consultoría</a></li>
-								<li class="dropdown">
-									<a href="#" class="dropdown-toggle effect-3" data-toggle="dropdown">Capacitación <b class="caret"></b></a>
-									<ul class="dropdown-menu" style="font-size: 1.1em;font-weight: 500">
-										<li class=""><a href="dcd.php" class="">DIPLOMADO EN COMPETENCIAS DOCENTES</a></li>
-										<li><a href="die.php">DIPLOMADO EN INSTITUCIONES EDUCATIVAS</a></li>
-									</ul>
-								</li>
-								<li><a href="contact.php" class="effect-3">Contacto</a></li>
-								<li><a href="nosotros.php" class="effect-3">Nosotros</a></li>
-								<li><a href="http://iineed.edu.mx/sistemaescolar/public" class="effect-3">Acceso Sistema</a></li>
-							</ul>
-						</nav>
-					</div>
-					<!--/.navbar-collapse-->
-					<!--/.navbar-->
-				</div>
-			</nav>
-		</div>
-	</div>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+        <div class="content white agile-info">
+            <nav class="navbar navbar-default" role="navigation">
+                <div class="container ">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse"
+                            data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php">
+                            <img src="images/logo.png" width="150px"><br><br>
+                        </a>
+                    </div>
+                    <!--/.navbar-header-->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <nav class="link-effect-2" id="link-effect-2">
+                            <ul class="nav navbar-nav">
+                                <li class="active  animated rotateInUpLeft"><a href="index.php"
+                                        class="effect-3">Inicio</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle effect-3" data-toggle="dropdown">Posgrados <b
+                                            class="caret"></b></a>
+                                    <ul class="dropdown-menu" style="font-size: 1.1em;font-weight: 500">
+                                        <li class=""><a href="md2.php" class="">Maestría en Docencia</a></li>
+                                        <li class=""><a href="md2l.php" class="">Maestría en Docencia en linea</a></li>
+                                        <li><a href="mpe2.php">Maestría en Psicología Educativa</a></li>
+                                        <li><a href="mgpp2.php">Maestría en Gestión de Políticas Públicas</a>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li><a href="consultoria.php" class="effect-3">Consultoría</a></li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle effect-3" data-toggle="dropdown">Capacitación <b
+                                            class="caret"></b></a>
+                                    <ul class="dropdown-menu" style="font-size: 1.1em;font-weight: 500">
+                                        <li class=""><a href="dcd.php" class="">DIPLOMADO EN COMPETENCIAS DOCENTES</a>
+                                        </li>
+                                        <li><a href="die.php">DIPLOMADO EN INSTITUCIONES EDUCATIVAS</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="contact.php" class="effect-3">Contacto</a></li>
+                                <li><a href="nosotros.php" class="effect-3">Nosotros</a></li>
+                                <li><a href="http://iineed.edu.mx/sistemaescolar/public" class="effect-3">Acceso
+                                        Sistema</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <!--/.navbar-collapse-->
+                    <!--/.navbar-->
+                </div>
+            </nav>
+        </div>
+    </div>
