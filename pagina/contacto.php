489ba12250e986cc67f00545c4910e7ecf5702ad
Tania <ttania_torres@hotmail.com>
2024-01-30 22:24:23 -0600

    cambios correo

--- pagina/contacto.php
+++ pagina/contacto.php
@@ -3,27 +3,32 @@ include "header.php";
 ?>
 <br>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         $("#contacto").removeClass("grey-text");
         $("#contacto").addClass("blue-text text-darken-4");
     });
 </script>
-<div class="divsection">
+<?php
+if (isset($_GET["status"])) {
+?>
+    <div class="blue darken-4 white-text center-align p-4">Tu mensaje se ha enviado correctamente, nos pondremos en contacto contigo a la brevedad</div>
+<?php
+}
+?> <div class="divsection">
     <div class="row section">
         <div class="col m6 s12  left-align">
             <form action="enviarcorreo.php" method="get" id="formcontacto">
-            <input type="text" name="asunto" placeholder="Asunto" required="">
-            <input type="text" name="nombre" placeholder="Nombre" required="">
-            <input type="email" name="correo" placeholder="Correo" required="">
-            <textarea name="mensaje" placeholder="Mensaje..." required="" style="border-radius:10px;height:8em" ></textarea>
-            <div class="amber darken-4 white-text center-align col m12 s12 z-depth-5 waves-effect waves-light modal-trigger" onclick="$('#formcontacto').submit()" style="padding:10px;border-radius:10px"><h6>Enviar</h6></div>
+                <input type="text" name="asunto" placeholder="Asunto" required="">
+                <input type="text" name="nombre" placeholder="Nombre" required="">
+                <input type="email" name="correo" placeholder="Correo" required="">
+                <textarea name="mensaje" placeholder="Mensaje..." required="" style="border-radius:10px;height:8em"></textarea>
+                <div class="amber darken-4 white-text center-align col m12 s12 z-depth-5 waves-effect waves-light modal-trigger" onclick="$('#formcontacto').submit()" style="padding:10px;border-radius:10px">
+                    <h6>Enviar</h6>
+                </div>
         </div>
         <div class="col m6 s12">
             <div class="map">
-                <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.196857205904!2d-106.08365829947508!3d28.623861552791016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea5cb6c1dacf69%3A0x269dabb81eb0963b!2sC.%20Ernesto%20Talavera%201208%2C%20Zarco%2C%2031020%20Chihuahua%2C%20Chih.!5e0!3m2!1ses!2smx!4v1675374151153!5m2!1ses!2smx"
-                width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.196857205904!2d-106.08365829947508!3d28.623861552791016!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x86ea5cb6c1dacf69%3A0x269dabb81eb0963b!2sC.%20Ernesto%20Talavera%201208%2C%20Zarco%2C%2031020%20Chihuahua%2C%20Chih.!5e0!3m2!1ses!2smx!4v1675374151153!5m2!1ses!2smx" width="600" height="350" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
         </div>
     </div>
@@ -78,10 +83,10 @@ include "header.php";
     <div class="row section blue lighten-5 black-text" style="padding:50px">
         <div class="col m12  s12 center-align" style="padding:50px">
             <h5><b class="blue-text text-darken-4">IINEED Es una institución educativa con CCT 08DPSU5050 que desde 2008 imparte programas de posgrado, acreditados por la Secretaría de Educación y Deporte y por la Dirección General de Profesores de la SEP. Además, está acreditada como organismo Evaluador(OAE) por el Consejo para la Evaluación de la Educación Tipo Medio Superior (COPEEMS)</b></h5>
-            
+
         </div>
     </div>
 </div>
-        <?php
+<?php
 include "footer.php";
 ?>
--- pagina/enviarcorreo.php
+++ pagina/enviarcorreo.php
@@ -1,11 +1,10 @@
 <?php
-    ini_set( 'display_errors', 1 );
-    error_reporting( E_ALL );
-    $from = "admin@iineed.edu.mx";
-    $to = "admin@iineed.edu.mx";
-    $subject = "Correo enviado desde página Web";
-    $message = "Asunto: <br>".$_GET["asunto"]."<br><br>Nombre: ".$_GET["nombre"]."<br><br>Correo: ".$_GET["correo"]."<br><br>Mensaje: ".$_GET["mensaje"]."<br><br>";
-    $headers = "From:" . $from;
-    mail($to,$subject,$message, $headers);
-    
-?>
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+$from = "admin@iineed.edu.mx";
+$to = "admin@iineed.edu.mx";
+$subject = "Correo enviado desde contacto Web";
+$message = "Asunto: \n" . $_GET["asunto"] . "\n\nNombre:\n " . $_GET["nombre"] . "\n\nCorreo: \n" . $_GET["correo"] . "\n\nMensaje: \n" . $_GET["mensaje"] . "\n\n";
+$headers = "From:" . $from;
+mail($to, $subject, $message, $headers);
+header('Location: contacto.php?status="enviado"');
